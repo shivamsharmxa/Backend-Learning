8087c333d81f2fb52772f792a39bc37fb85fceec
Shivam Sharma <shivam.sharma@mesdo.in>
2026-01-12 01:05:21 +0530

    Convert it into Express

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
-const http = require('http');
-const app = require('./app');
+const express = require('express');
+const app = express();
+
+app.use(express.json());
 
 const PORT = 3000;
 
-const server = http.createServer(app);
-
-server.listen(PORT, () => {
+app.get('/', (req, res) => {
+    res.send('Server is running')
+});
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
